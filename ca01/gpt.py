88a2ee5054fcfbf523a5ab8af9f9b4a70bce8212
AnUnoriginalSam <samherman345@gmail.com>
2023-03-26 19:09:22 -0400

    Update gpt.py

--- ca01/gpt.py
+++ ca01/gpt.py
@@ -42,6 +42,17 @@ class GPT():
             temperature=0.8,
         )
 
+    def SamResponse(self,prompt):
+        ''' Generate a GPT response '''
+        completion = openai.Completion.create(
+            engine=self.model_engine,
+            prompt=prompt,
+            max_tokens=1024,
+            n=1,
+            stop=None,
+            temperature=0.8,
+        )
+
         response = completion.choices[0].text
         return response
     
