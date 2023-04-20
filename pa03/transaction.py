d7e9b24a6aadd051eedf044f5cfc1d2354226323
Zawack0 <zawackiconnor@gmail.com>
2023-04-19 22:39:33 -0400

    forgot this bit

--- pa03/transaction.py
+++ pa03/transaction.py
@@ -43,16 +43,17 @@ class Transaction:
     def get_menu(self):
         '''returns menu'''
         menu = '''0. quit
-1. show categories
-2. add category
-3. modify category
-4. show transactions
-5. add transaction
-6. delete transaction
-7. summarize transactions by date
-8. summarize transactions by month
-9. summarize transactions by year
-10. summarize transactions by category'''
+                1. show categories
+                2. add category
+                3. modify category
+                4. show transactions
+                5. add transaction
+                6. delete transaction
+                7. summarize transactions by date
+                8. summarize transactions by month
+                9. summarize transactions by year
+                10. summarize transactions by category'''
+        return menu
 
     
 
