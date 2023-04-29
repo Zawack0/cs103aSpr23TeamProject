3243c3e9c07cd65e89245802df4e77f0b83afdbb
Zawack0 <zawackiconnor@gmail.com>
2023-04-29 16:47:46 -0400

    transaction skeleton

--- pa04/firstapp/app.js
+++ pa04/firstapp/app.js
@@ -6,6 +6,7 @@ const logger = require('morgan');
 const layouts = require("express-ejs-layouts");
 const pw_auth_router = require('./routes/pwauth')
 const toDoRouter = require('./routes/todo');
+const transRouter = require('./routes/transaction')
 
 const User = require('./models/User');
 
@@ -105,6 +106,7 @@ app.get('/about',
 )
 
 app.use(toDoRouter);
+app.use(transRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ pa04/firstapp/models/TransactionItem.js
@@ -0,0 +1,14 @@
+//THIS IS SUPER WRONG JUST BARE WITH ME
+'use strict';
+const mongoose = require( 'mongoose' );
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+var toDoItemSchema = Schema( {
+  item: String,
+  completed: Boolean,
+  createdAt: Date,
+  userId: ObjectId
+} );
+
+module.exports = mongoose.model( 'TransactionItem', toDoItemSchema );
